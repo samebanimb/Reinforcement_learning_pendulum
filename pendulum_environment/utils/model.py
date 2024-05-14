9ca980ed79f0f75029e14fc575e0422d6edc9230
Same Banimb <samebanimb15@hotmail.com>
2024-05-14 17:48:47 +0200

    Add of environment as a package

---
+++ pendulum_environment/utils/model.py
@@ -0,0 +1,54 @@
+"""
+Class, which contains
+ the model and subsequently
+the parameters of the model
+
+"""
+
+from typing import Union
+from math import sin, cos
+import numpy as np
+
+
+def Model(states: np.array, action: Union[int, np.ndarray]) -> np.ndarray:
+    """_summary_
+
+    Args:
+        states (np.array): position, angle, velocity, angular velocity
+        action (Union[int, np.ndarray]): voltage of the motor
+
+    Returns:
+        np.ndarray: _description_
+    """
+    x = states[1, 1]
+    theta = states[2, 1]
+    x_dot = states[3, 1]
+    theta_dot = states[4, 1]
+    gravity = 9.03788858603972
+    mass_cart = 0.94
+    mass_pole = 0.127
+    length = 0.2066
+    damping_factor = 8.28866833710732
+    gain_factor = 1.16342493738728
+
+    new_states = np.array(
+        [
+            x,
+            theta,
+            (
+                mass_pole * length * sin(theta) * theta_dot**2
+                + mass_pole * gravity * sin(theta) * cos(theta)
+                + (-damping_factor * x_dot + gain_factor * action)
+            )
+            / (mass_cart + mass_pole * sin(theta) ** 2),
+            -(
+                mass_pole * length * sin(theta) * cos(theta) * theta_dot**2
+                + (mass_pole + mass_cart) * gravity * sin(theta)
+                + cos(theta) * (-damping_factor * x_dot + gain_factor * action)
+            )
+            / (length * (mass_cart + mass_pole * sin(theta) ^ 2)),
+        ]
+    )
+
+    new_states = np.reshape(new_states, (4, 1))
+    return new_states
