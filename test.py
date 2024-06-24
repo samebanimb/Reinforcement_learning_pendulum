eb4efe0b6559903d9bd4595652d2479ef70f4ec0
Same Banimb <samebanimb15@hotmail.com>
2024-06-24 18:02:09 +0200

    delete unnecessary files

--- test.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    import pendulum_environment  # noqa
-    import gym
-
-    env = gym.make("pendulum_environment/Pendulum-v0", render_mode="human")
-    env.reset()
-    env.render()
