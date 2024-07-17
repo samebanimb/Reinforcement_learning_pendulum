0e3957fec8c859b89f0772318b1111506a068068
Same Banimb <samebanimb15@hotmail.com>
2024-07-17 15:56:43 +0200

    remove penalty after stabilization and termination

--- pendulum_environment/envs/pendulum_stabilization.py
+++ pendulum_environment/envs/pendulum_stabilization.py
@@ -134,8 +134,8 @@ class Pendulum_Stabilization(gym.Env):
         #        )
         #        if cos(theta) < 0:
         #            reward -= 0.05 * cos(theta)
-        if terminated:
-            reward -= 100
+        # if terminated:
+        #    reward -= 100
         if not terminated:
             reward += 1
 
