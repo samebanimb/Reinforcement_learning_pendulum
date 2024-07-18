90ffeaec6e692ff649073ac6e1e7e3934e1d976d
Same Banimb <samebanimb15@hotmail.com>
2024-07-18 17:52:40 +0200

    limit max episode length to 25 seconds

--- pendulum_environment/__init__.py
+++ pendulum_environment/__init__.py
@@ -3,13 +3,13 @@ from gym.envs.registration import register
 register(
     id="pendulum_environment/Pendulum-v0",
     entry_point="pendulum_environment.envs:Pendulum",
-    max_episode_steps=600,
+    max_episode_steps=500,
 )
 
 register(
     id="pendulum_environment_evaluation/Pendulum_eval-v0",
     entry_point="pendulum_environment.envs:Pendulum_Evaluation",
-    max_episode_steps=600,
+    max_episode_steps=500,
 )
 register(
     id="pendulum_environment_stabilization/Pendulum_eval-v0",
